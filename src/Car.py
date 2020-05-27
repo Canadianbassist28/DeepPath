9698dbf1272a85b8d1063c8679be9ecc15b5675c
Canadianbassist28 <Canadianbassist28@gmail.com>
2020-05-27 01:47:22 -0400

    added functionality for reverse motion if target if behind car, car should back up and turn (needs polish) +added initial code for collision objects to blocks

--- src/Car.py
+++ src/Car.py
@@ -1,8 +1,8 @@
 import pygame as pg
 import math
 
-CAR_MAX_TURN_RATE = 25
-CAR_MAX_VELOCITY = .25
+CAR_MAX_TURN_RATE = 1000
+CAR_MAX_VELOCITY = 2.5
 
 class Car(pg.sprite.Sprite):
 
@@ -14,6 +14,7 @@ class Car(pg.sprite.Sprite):
 		self.pos = pg.math.Vector2((screen.get_width() // 2, screen.get_height() // 2))
 		self.rot = 0
 		self.forward = pg.math.Vector2(math.cos(math.radians(self.rot)), math.sin(math.radians(self.rot)))
+		self.right = self.forward.rotate(90)
 
 		self.img = pg.Surface((self.width, self.height), pg.SRCALPHA)
 		self.img.fill((255,255,255))
@@ -21,19 +22,26 @@ class Car(pg.sprite.Sprite):
 		self.rect = self.img.get_rect()
 		self.rect.center = self.pos
 
-	def update(self, screen):
+	def update(self, screen, walls):
 		img_copy = self.img.copy()
 		mouse_x, mouse_y  = pg.mouse.get_pos()
 		self.forward = pg.math.Vector2(math.sin(math.radians(self.rot)), math.cos(math.radians(self.rot)))
+		self.right = self.forward.rotate(90)
 
 		#--------Update speed--------
-		heading = pg.mouse.get_pos() - self.pos
-		dist = heading.length()
-		velocity = max(min(dist * .005, CAR_MAX_VELOCITY), -CAR_MAX_VELOCITY)
+		velocity = self.__find_velocity()
+		dist = (pg.mouse.get_pos() - self.pos).length()
 
 		if dist > 10:
+			old_pos = self.pos
 			self.pos += self.forward * velocity
 			self.rect.center = self.pos
+			collided = pg.sprite.spritecollideany(self, walls)
+
+			# # TODO: finish collision detection
+			# if collided is not None:
+			# 	self.rect.center = self.pos = old_pos
+			# 	if
 
 		#--------Update Rotation--------
 		target_rot = math.atan2(mouse_x - self.pos[0], mouse_y - self.pos[1]) * (180 // math.pi)
@@ -41,7 +49,7 @@ class Car(pg.sprite.Sprite):
 		rot_dist = max(min(rot_dist, CAR_MAX_TURN_RATE), -CAR_MAX_TURN_RATE)
 
 		if dist > 10:
-			self.rot += (rot_dist / 100)
+			self.rot += (rot_dist / 35)
 			self.rot = self.rot % 360
 
 		img_copy = pg.transform.rotate(img_copy, self.rot)
@@ -51,3 +59,25 @@ class Car(pg.sprite.Sprite):
 		# Draw Car
 		screen.blit(img_copy, self.rect)
 		pg.draw.line(screen, (255,0,0),self.pos, (self.forward * 25 + self.pos))
+		pg.draw.line(screen, (0,0,255),self.pos, (self.right * 25 + self.pos))
+
+	def __find_velocity(self):
+		mouse_pos = pg.mouse.get_pos()
+		heading = mouse_pos - self.pos
+		dist = heading.length()
+		velocity = max(min(dist * .05, CAR_MAX_VELOCITY), -CAR_MAX_VELOCITY)
+
+		#-----determine forward or reverse-----
+		m = ((self.right + self.pos)[1] - self.pos[1]) / ((self.right + self.pos)[0] - self.pos[0] + .000000001)
+		b = self.pos[1] - (m * self.pos[0])
+
+		sign = True if  (self.right + self.pos)[0] < self.pos[0] else False # determine if upside-down
+		# print(f"{sign} | {(mouse_pos - self.pos)}")
+		if (mouse_pos[1] + 20 > (m * mouse_pos[0] + b)):
+			if sign:
+				velocity *= -1
+		else:
+			if not sign:
+				velocity *= -1
+
+		return -velocity
